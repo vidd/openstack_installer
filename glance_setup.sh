85ba3c5ccb67ceb2d3e45971206590830d028598
vidd <vidd@hotmail.com>
2011-11-17 11:45:59 -0500

    Fixed Glance Scrubber to use MySQL

--- glance_setup.sh
+++ glance_setup.sh
@@ -11,6 +11,12 @@ sed -e "s,%GLANCE_DATABASE_USER%,$GLANCE_DATABASE_USER,g" -i /etc/glance/glance-
 sed -e "s,%GLANCE_DATABASE_PASS%,$GLANCE_DATABASE_PASS,g" -i /etc/glance/glance-registry.conf
 sed -e "s,%GLANCE_DATABASE_HOST%,$GLANCE_DATABASE_HOST,g" -i /etc/glance/glance-registry.conf
 
+sed -i 's/sqlite:\/\/\/\/var\/lib\/glance\/glance.sqlite/mysql:\/\/%GLANCE_DATABASE_USER%:%GLANCE_DATABASE_PASS%@%GLANCE_DATABASE_HOST%\/%GLANCE_DATABASE_NAME%/g' /etc/glance/glance-scrubber.conf
+sed -e "s,%GLANCE_DATABASE_NAME%,$GLANCE_DATABASE_NAME,g" -i /etc/glance/glance-scrubber.conf
+sed -e "s,%GLANCE_DATABASE_USER%,$GLANCE_DATABASE_USER,g" -i /etc/glance/glance-scrubber.conf
+sed -e "s,%GLANCE_DATABASE_PASS%,$GLANCE_DATABASE_PASS,g" -i /etc/glance/glance-scrubber.conf
+sed -e "s,%GLANCE_DATABASE_HOST%,$GLANCE_DATABASE_HOST,g" -i /etc/glance/glance-scrubber.conf
+
 glance-manage db_sync
 glance-control all restart
 
