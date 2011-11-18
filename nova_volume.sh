5fdb9bc51c1e70afcedaa22ad22ea7dcc52ec94a
vidd <vidd@hotmail.com>
2011-11-17 22:49:07 -0500

    fixed logic flow

--- nova_volume.sh
+++ nova_volume.sh
@@ -18,18 +18,20 @@ done
 LVM_SETUP=z
 until [ "$LVM_SETUP" = "y" -o "$LVM_SETUP" = "n" ]; do
 	echo "Do we need to set up the LVM partion for Nova-Volume?"
-	echo "If unsure enter which drve to pick hit <?>"
-	echo "Do not include the /dev/"
 	read LVM_SETUP
-
 	case $LVM_SETUP in
 		y ) echo "Enter partition for Nova-Volume"
+			echo "If unsure enter which drve to pick hit <?>"
+			echo "Do not include the /dev/"
 			read LVM_drive
+				until [ $LVM_drive" != "?" ]; do 
+					fdisk -l
+					read LVM_drive
+				done 
 			pvcreate /dev/$LVM_drive
 			vgcreate nova-volumes /dev/$LVM_drive ;;
-		? ) fdisk -l ;;
 		n ) echo "OK, Moving on then" ;;
-		* ) echo "Please enter y,n, or ?" ;;
+		* ) echo "Please enter y or n" ;;
 	esac
 done
 
