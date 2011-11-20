611c962da9bce91c22c8a8ecf007c19f5af06377
vidd <vidd@hotmail.com>
2011-11-20 15:46:35 -0500

    corrected vncproxy

--- nova-preload.sh
+++ nova-preload.sh
@@ -67,7 +67,7 @@ add_nova_flag "--public_interface=eth0"
 add_nova_flag "## VNC Proxy Service"
 add_nova_flag "#--vncproxy_wwwroot=%PATH_TO_noVNC%"
 add_nova_flag "#--vncproxy_port=6080"
-add_nova_flag "#--vncproxy_host=$NOVA_HOST_IP"
+add_nova_flag "#--vncproxy_host=0.0.0.0"
 
 echo "Please hit <ENTER> to continue"
 read DUMMY
