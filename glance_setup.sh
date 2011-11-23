a7c36e89b145a64030e6781a5325faf531ea3a0e
vidd <vidd@hotmail.com>
2011-11-23 00:52:08 -0500

    fixed issue with replacing sqlite url

--- glance_setup.sh
+++ glance_setup.sh
@@ -5,13 +5,13 @@
 echo "installing Glance"
 apt-get install -y glance python-httplib2 python-mysqldb
 . database
-sed -i 's/sqlite:\/\/\/\/var\/lib\/glance\/glance.sqlite/mysql:\/\/%GLANCE_DATABASE_USER%:%GLANCE_DATABASE_PASS%@%GLANCE_DATABASE_HOST%\/%GLANCE_DATABASE_NAME%/g' /etc/glance/glance-registry.conf
+sed -i 's/sql_connection = /sql_connection = mysql:\/\/%GLANCE_DATABASE_USER%:%GLANCE_DATABASE_PASS%@%GLANCE_DATABASE_HOST%\/%GLANCE_DATABASE_NAME% \n\ #sql_connection = /g' /etc/glance/glance-registry.conf
 sed -e "s,%GLANCE_DATABASE_NAME%,$GLANCE_DATABASE_NAME,g" -i /etc/glance/glance-registry.conf
 sed -e "s,%GLANCE_DATABASE_USER%,$GLANCE_DATABASE_USER,g" -i /etc/glance/glance-registry.conf
 sed -e "s,%GLANCE_DATABASE_PASS%,$GLANCE_DATABASE_PASS,g" -i /etc/glance/glance-registry.conf
 sed -e "s,%GLANCE_DATABASE_HOST%,$GLANCE_DATABASE_HOST,g" -i /etc/glance/glance-registry.conf
 
-sed -i 's/sqlite:\/\/\/\/var\/lib\/glance\/glance.sqlite/mysql:\/\/%GLANCE_DATABASE_USER%:%GLANCE_DATABASE_PASS%@%GLANCE_DATABASE_HOST%\/%GLANCE_DATABASE_NAME%/g' /etc/glance/glance-scrubber.conf
+sed -i 's/sql_connection = /mysql:\/\/%GLANCE_DATABASE_USER%:%GLANCE_DATABASE_PASS%@%GLANCE_DATABASE_HOST%\/%GLANCE_DATABASE_NAME% \n\ #sql_connection = /g' /etc/glance/glance-scrubber.conf
 sed -e "s,%GLANCE_DATABASE_NAME%,$GLANCE_DATABASE_NAME,g" -i /etc/glance/glance-scrubber.conf
 sed -e "s,%GLANCE_DATABASE_USER%,$GLANCE_DATABASE_USER,g" -i /etc/glance/glance-scrubber.conf
 sed -e "s,%GLANCE_DATABASE_PASS%,$GLANCE_DATABASE_PASS,g" -i /etc/glance/glance-scrubber.conf
--- keystone_setup.sh
+++ keystone_setup.sh
@@ -10,7 +10,7 @@ apt-get install -y keystone python-mysqldb mysql-client curl
 sed -i 's/verbose = False/verbose = True/g' /etc/keystone/keystone.conf
 #sed -i 's/log_file = keystone.log/log_file = \/var\/log\/keystone.log/g' /etc/keystone/keystone.conf
 sed -i 's/default_store = sqlite/default_store = mysql/g' /etc/keystone/keystone.conf
-sed -i 's/sqlite:\/\/\/\/var\/lib\/keystone\/keystone.db/mysql:\/\/%KEYSTONE_DATABASE_USER%:%KEYSTONE_DATABASE_PASS%@%KEYSTONE_DATABASE_HOST%\/%KEYSTONE_DATABASE_NAME%/g' /etc/keystone/keystone.conf
+sed -i 's/sql_connection = /mysql:\/\/%KEYSTONE_DATABASE_USER%:%KEYSTONE_DATABASE_PASS%@%KEYSTONE_DATABASE_HOST%\/%KEYSTONE_DATABASE_NAME% \n\ #sql_connection = /g' /etc/keystone/keystone.conf
 sed -e "s,%KEYSTONE_DATABASE_NAME%,$KEYSTONE_DATABASE_NAME,g" -i /etc/keystone/keystone.conf
 sed -e "s,%KEYSTONE_DATABASE_USER%,$KEYSTONE_DATABASE_USER,g" -i /etc/keystone/keystone.conf
 sed -e "s,%KEYSTONE_DATABASE_PASS%,$KEYSTONE_DATABASE_PASS,g" -i /etc/keystone/keystone.conf
